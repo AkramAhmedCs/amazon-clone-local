2730b54b4f1b709395b0ccaa6dde520945030372
Akram_Ahmed <akramaboukandil@gmail.com>
2025-04-28 07:35:12 +0300

    saved the cart to local storage, if it doent exist we give it a default value

--- data/cart.js
+++ data/cart.js
@@ -1,13 +1,11 @@
-export let cart = [{
-  productID:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-  quantity:2
-},
-{
-  productID:'15b6fc6f-327a-4ec4-896f-486349e85a3d',
-  quantity:1
-
+export let cart = JSON.parse(localStorage.getItem('cart'));
+if(!cart){
+  cart = [];
 }
-];
+
+function saveToStorage(){
+  localStorage.setItem('cart',JSON.stringify(cart));
+}
 export function addToCart(productID){
 
   let matchingItem;
@@ -23,6 +21,7 @@ export function addToCart(productID){
       quantity:1
     });
   }
+  saveToStorage();
 
 }
 export function removeFromCart(productID){
@@ -33,4 +32,5 @@ export function removeFromCart(productID){
     }
   }))
   cart = newCart;
+  saveToStorage();
 }
