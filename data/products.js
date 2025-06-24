7399b24d8beba34e15537414a9b033ec59b00ae2
Akram_Ahmed <akramaboukandil@gmail.com>
2025-06-24 12:13:20 +0300

    convert products into classes

--- data/products.js
+++ data/products.js
@@ -1,4 +1,4 @@
- 
+ import { formatMoney } from "../scripts/utils/money.js";
  export function getProduct(productID){
   let matchingProduct;
   products.forEach((product)=>{
@@ -8,6 +8,27 @@
   })
   return matchingProduct;
  }
+
+ class Products{
+  id;
+  image;
+  name;
+  rating;
+  priceCents;
+  constructor(productDetails){
+    this.id = productDetails.id;
+    this.image = productDetails.image;
+    this.name = productDetails.name;
+    this.rating = productDetails.rating;
+    this.priceCents = productDetails.priceCents;
+  }
+  getStarsUrl (){
+   return `images/ratings/rating-${this.rating.stars*10}.png`
+  }
+  getPrice(){
+   return ` $${formatMoney(this.priceCents)}`
+  }
+}
  export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
@@ -667,4 +688,7 @@
       "mens"
     ]
   }
-];
+].map((productDetails)=>{
+  return new Products(productDetails)
+});
+console.log(products)
--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -36,7 +36,7 @@ cart.forEach((cartItem) => {
                   ${matchingProduct.name}
                 </div>
                 <div class="product-price js-product-price">
-                  $${formatMoney(matchingProduct.priceCents)}
+                  ${matchingProduct.getPrice()}
                 </div>
                 <div class="product-quantity
                   js-product-quantity-${matchingProduct.id}">
