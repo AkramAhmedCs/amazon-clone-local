e1eaaf6fb1f806ba9f6dc6fc4e55edfda405b55a
Akram_Ahmed <akramaboukandil@gmail.com>
2025-06-28 15:43:28 +0300

    add backend practice file

--- data/backend-practice.js
+++ data/backend-practice.js
@@ -1,3 +1,11 @@
 const xhr = new XMLHttpRequest()
-xhr.open('GET','https://supersimplebackend.dev')
+xhr.addEventListener('load',()=>{
+  console.log(xhr.response)
+});
+xhr.open('GET','https://supersimplebackend.dev/images/apple.jpg')
 xhr.send();
+
+
+//status code with 4 or 5 === failed
+// 4 is our prob , 5 is backend prob
+//2 is succesful
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,8 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { rednderPaymentSummary } from "./checkout/paymentSummary.js";
 import '../data/car.js';
+import '../data/backend-practice.js'
+
 //import '../data/cart-class.js' 
 renderOrderSummary();
 rednderPaymentSummary();
